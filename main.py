af8282b01e9938edc71b8bba7217ecd0ce7fb828
akshayaavivekanandhan <114179590+akshayaavivekanandhan@users.noreply.github.com>
2022-10-02 19:58:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+Humidity= random.randint(60,70)
+Temperature=random.randint(60,70)
+if(Temperature<65 and Humidity<35):
+  print("NO HARM")
+elif (Temperature<65 or Humidity<35):
+  if (Temperature<65 or Humidity>35):
+    print("HUMIDITY ALERT")
+  elif(Temperature>65 or Humidity<35):
+    print("TEMPERATURE ALERT")
+else:
+  print("THE AREA SHOULD BE CLEARED IMMEDIATELY")
